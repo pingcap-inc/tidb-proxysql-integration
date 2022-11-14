0308c264d4d444d0b278745b8b80ac98f39e7cfe
Icemap <cheese.javaee@gmail.com>
2022-11-14 19:40:08 +0800

    [Update] temp remove mycli login

--- example/tidb-cloud-connect/tidb-cloud-connect.sh
+++ example/tidb-cloud-connect/tidb-cloud-connect.sh
@@ -12,4 +12,4 @@ docker-compose exec proxysql sh -c "mysql -uadmin -padmin -h127.0.0.1 -P6032 < .
 
 docker-compose restart
 
-mycli -u 'HZ5E7ifaDEjJTsh.root' -h 127.0.0.1 -P 16033 -D test --ssl-ca=/etc/ssl/certs/ca-certificates.crt --ssl-verify-server-cert
+# mycli -u 'HZ5E7ifaDEjJTsh.root' -h 127.0.0.1 -P 16033 -D test --ssl-ca=/etc/ssl/certs/ca-certificates.crt --ssl-verify-server-cert
